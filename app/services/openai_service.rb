9dd7b89dc0d5d266c81c9e29f3f43d707f765636
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-06-07 12:16:30 +0200

    api working

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -14,11 +14,13 @@ class OpenaiService
       parameters: {
           model: "gpt-3.5-turbo", # Required.
           messages: [{ role: "user", content: prompt }], # Required.
-          temperature: 1,
+          temperature: 1.6,
           stream: false,
-					max_tokens: 100 # might want to check this
+					max_tokens: 3200 # keep buffer for prompt tokens
       })
-    # you might want to inspect the response and see what the api is giving you
+
     return response["choices"][0]["message"]["content"]
   end
 end
+# create test response in rails console
+#response = OpenaiService.new("You are a world class author for children's books. Help me create a bedtime story to read to my child that is based on the hero's journey. It should be about a one-eyed crocodile tht is looking for water on Mars.").call
