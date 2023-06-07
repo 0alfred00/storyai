bcee46426bac649e31057b72ecaba991bff4696e
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-06-07 16:42:15 +0200

    api working

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,5 +1,6 @@
 # app/services/openai_service.rb
 require "openai"
+require "json"
 
 class OpenaiService
   attr_reader :client, :prompt
@@ -19,8 +20,25 @@ class OpenaiService
 					max_tokens: 3200 # keep buffer for prompt tokens
       })
 
-    return response["choices"][0]["message"]["content"]
+    if response["error"]
+      return response["error"]["message"]
+    else
+      return response["choices"][0]["message"]["content"]
+      # return JSON.parse(response["choices"][0]["message"]["content"])
+    end
   end
 end
+
+=begin
 # create test response in rails console
-#response = OpenaiService.new("You are a world class author for children's books. Help me create a bedtime story to read to my child that is based on the hero's journey. It should be about a one-eyed crocodile tht is looking for water on Mars.").call
+prompt = "Please create the following things for me:
+- Title for a bedtime story, not longer than 5 words
+- Summary of a bedtime story not longer than 5 sentences
+- Summary of a bedtime story not longer than one sentence
+
+I would like the answer to be in JSON format, with the following keys: body, title, summary and follow_up_summary.
+The title key should contain the title of the story, the summary a one liner summary of the story and the follow_up_summary a paragraph summary of the story. The response should be a JSON and JSON only!
+"
+response = OpenaiService.new(prompt).call
+JSON.parse(response)
+=end
