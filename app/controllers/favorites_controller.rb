7c4bf378611da65e19cc6991ccb750413ca6fa0b
Natalie <130673935+natalie-tsering@users.noreply.github.com>
2023-06-08 16:29:41 +0200

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,10 +4,3 @@ class FavoritesController < ApplicationController
     @stories = current_user.favorites
   end
 end
-
-newfav = Favorite.create(user_id: 11, story_id: 1)
-newfav.save
-newfav = Favorite.create(user_id: 11, story_id: 2)
-newfav.save
-newfav = Favorite.create(user_id: 11, story_id: 3)
-newfav.save
