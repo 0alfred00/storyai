429daf639422d5de4ffeeed4081caa696ced89ac
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-06-08 16:23:45 +0200

    wip

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  def index
+    # shows favorites of current user
+    @stories = current_user.favorites
+  end
 end
+
+newfav = Favorite.create(user_id: 11, story_id: 1)
+newfav.save
+newfav = Favorite.create(user_id: 11, story_id: 2)
+newfav.save
+newfav = Favorite.create(user_id: 11, story_id: 3)
+newfav.save
