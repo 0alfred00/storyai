ad11a54feb16f13b48238073b4e410466ef24651
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-06-15 20:16:36 +0200

    d

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # sidekiq setup for heroku
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
