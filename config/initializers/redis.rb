37d8ae36ec0eaddb9cd27e6a82225f99a8c58f13
Alfred Seeliger <seeliger.alfred@gmail.com>
2023-06-15 19:14:25 +0200

    edited redis cloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,13 @@
-# sidekiq setup for heroku
+$redis = Redis.new
+url = ENV["REDISCLOUD_URL"]
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
-end
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  $redis = Redis.new(:url => url)
 end
